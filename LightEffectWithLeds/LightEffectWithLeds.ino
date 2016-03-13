5f78c00292b80f037449860bab8af4a62cabffad
Alison Souza <alisonmsouza@yahoo.com.br>
2016-03-13 12:42:37 -0300

    speed control

--- LightEffectWithLeds/LightEffectWithLeds.ino
+++ LightEffectWithLeds/LightEffectWithLeds.ino
@@ -1,10 +1,13 @@
 byte ledPin[10] = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13}; // cria um array para os pinos dos LEDs
-int ledDelay(50);                                     // intervalo entre as alterações
+int ledDelay;                                         // intervalo entre as alterações
 boolean direction = true;                             // relacao com o indice do array
 int currentLED = 0;
 unsigned long changeTime;
+int potPin = A2;                                      // potenciometro
 
 void setup() {
+  //Serial.begin(9600);
+  
   // define todos os pinos como saída
   for (int x = 0; x < 10; x++) {
     pinMode(ledPin[x], OUTPUT);
@@ -13,6 +16,14 @@ void setup() {
 }
 
 void loop() {
+  // leitura do potenciometro
+  ledDelay = analogRead(potPin);
+
+  /*
+  Serial.print("ledDelay: ");
+  Serial.println(ledDelay);
+  */
+  
   // verifica se já transcorreram ledDelay ms desde
   if ((millis() - changeTime) > ledDelay) {
     changeLED();
